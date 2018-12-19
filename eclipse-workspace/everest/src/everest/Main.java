2e9f16143892aa85f24642712f2c9855c9ebc309
malyga <40186295+malyga@users.noreply.github.com>
2018-12-19 20:53:07 +0100

    dgfsdgs

---
+++ eclipse-workspace/everest/src/everest/Main.java
@@ -0,0 +1,72 @@
+package everest;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Main {
+	
+	static LinkedList<int[]> list = new LinkedList<int[]>();
+	
+	public static void main(String[] args) {
+		
+		readList();
+		
+		int[][] calculo = calcular();
+		for(int i = 0; i < calculo.length; ++i) {
+			System.out.println(calculo[i][0] + " " + calculo[i][1] + " " + calculo[i][2]);
+		}
+	}
+	
+	public static void readList() {
+		Scanner scanner = new Scanner(System.in);
+		String[] lectura;
+		String longitud;
+		do {
+			longitud = scanner.nextLine();
+			if(!longitud.equals("0")) {
+				lectura = scanner.nextLine().split(" ");
+				int[] temp = new int[lectura.length];
+				for(int i = 0; i < lectura.length; ++i) {
+					temp[i] = Integer.parseInt(lectura[i]);
+				}
+				list.add(temp);
+			}
+		} while (!longitud.equals("0"));
+	}
+	
+	public static int[][] calcular() {
+		int[][] output = new int[list.size()][3];
+		int i = 0;
+		for(int[] arr : list) {
+			int numGrupos = 0, menorGrupo= arr.length, mayorGrupo=-1, aux = 1;
+			
+			for(int j = 1; j < arr.length; ++j) {
+				
+				if(arr[j] < arr[j - 1]) {
+					numGrupos++;
+					if(aux < menorGrupo) {
+						menorGrupo = aux;
+					}
+					if(aux > mayorGrupo) {
+						mayorGrupo = aux;
+					}
+					aux = 1;
+				} else {
+					++aux;
+				}
+			}
+			numGrupos++;
+			if(aux < menorGrupo) {
+				menorGrupo = aux;
+			}
+			if(aux > mayorGrupo) {
+				mayorGrupo = aux;
+			}
+			
+			output[i] = new int[] {numGrupos, menorGrupo, mayorGrupo};
+			++i;
+		}
+		return output;
+	}
+
+}
---
+++ eclipse-workspace/hexagonos/src/hexagonos/Main.java
@@ -0,0 +1,39 @@
+package hexagonos;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Main {
+
+	static LinkedList<String[]> list = new LinkedList<String[]>();
+	
+	public static void main(String[] args) {
+		
+		readList2();
+		
+		
+		
+	}
+	
+	public static void readList() {
+		Scanner scanner = new Scanner(System.in);
+		String linea;
+		do {
+			linea = scanner.nextLine();
+			if (!linea.equals("0 0")) {
+				list.add(linea.split(" "));
+			}
+			
+		} while (!linea.equals("0 0"));
+	}
+	
+	public static void readList2() {
+		list.add(new String[] {"3", "*"});
+		list.add(new String[] {"4", "p"});
+	}
+	
+	public static void calcular() {
+		
+	}
+
+}
